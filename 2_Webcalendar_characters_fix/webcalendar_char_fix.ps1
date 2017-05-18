ffcffbfb1593267b0b4c0169247bfedc456354f5
Olivier Sarrat <olivier.sarrat@laposte.net>
2017-05-18 16:11:17 +0200

    Share webcalendar2odoo

---
+++ 2_Webcalendar_characters_fix/webcalendar_char_fix.ps1
@@ -0,0 +1,2 @@
+﻿(get-content -Encoding UTF8 webcalendar.ics) | foreach-object {$_ -replace "\?\x80\?", " "} | foreach-object {$_ -replace "\?\x99\?", " "} | foreach-object {$_ -replace "\?\x92\?", " "} | foreach-object {$_ -replace "\?\x85\?", " "} | set-content webcalendar.ics
+(get-content webcalendar.ics)  | set-content webcalendar.ics -Encoding UTF8
---
+++ README.md
@@ -0,0 +1,7 @@
+# Webcalendar2Odoo
+
+Procedure & tools for Webcalendar to Odoo calendar migration.
+
+Includes :
+- webcalendar exported ics charset error fix
+- thunderbird2odoo calendar migration through CSV
